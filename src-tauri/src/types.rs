1672bf4e3d838276fc6333d734c5c70c29727a8c
Adam Podkowinski <adampodkev@gmail.com>
2023-10-21 01:13:21 +0200

    Formatting

--- src-tauri/src/types.rs
+++ src-tauri/src/types.rs
@@ -1,7 +1,7 @@
 use google_drive3::hyper::client::HttpConnector;
 use google_drive3::hyper_rustls::HttpsConnector;
 use google_drive3::DriveHub;
-use tokio::sync::Mutex;
+use tauri::async_runtime::Mutex;
 
 pub type GHub = DriveHub<HttpsConnector<HttpConnector>>;
 
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-    import { invoke } from "@tauri-apps/api/tauri";
-    import { loggedIn, user } from "../../stores/gdrive";
+  import { invoke } from "@tauri-apps/api/tauri";
+  import { loggedIn, user } from "../../stores/gdrive";
 
-    let loading = false;
+  let loading = false;
 
-    const signIn = async () => {
-        loading = true;
-        $user = await invoke("sign_in");
-        loading = false;
-    };
+  const signIn = async () => {
+    loading = true;
+    $user = await invoke("sign_in");
+    loading = false;
+  };
 </script>
 
 <main class="bg-green-950 min-h-screen text-green-200 p-12">
-    <h1 class="text-5xl font-extrabold text-center mb-12">Welcome to GDrive Sync!</h1>
-    <a href="/">BACK</a>
-    <p>User: {$loggedIn} ({$user})</p>
-    {#if !loading}
-        <form on:submit|preventDefault={signIn}>
-            <button type="submit" class="px-3 py-2 bg-green-400 text-black rounded-xl">Submit</button>
-        </form>
-    {:else}
-        <p>Loading...</p>
-    {/if}
+  <h1 class="text-5xl font-extrabold text-center mb-12">Welcome to GDrive Sync!</h1>
+  <a href="/">BACK</a>
+  <p>User: {$loggedIn} ({$user})</p>
+  {#if !loading}
+    <form on:submit|preventDefault={signIn}>
+      <button type="submit" class="px-3 py-2 bg-green-400 text-black rounded-xl">Submit</button>
+    </form>
+  {:else}
+    <p>Loading...</p>
+  {/if}
 </main>
